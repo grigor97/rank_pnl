146148346051eeffaff16f0f7f88210b9d1325b1
Grigor Keropyan <goqorkeropyan@gmail.com>
2022-09-11 15:22:59 +0200

    added job

---
+++ job.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#SBATCH -o /dss/dsshome1/lxc02/ge79mok2/new1_rank_pnl/res/myjob1.out
+#SBATCH -D /dss/dsshome1/lxc02/ge79mok2/new1_rank_pnl/
+#SBATCH -J job1
+#SBATCH --get-user-env
+#SBATCH --clusters=cm2_tiny
+#SBATCH --partition=cm2_tiny
+#SBATCH --nodes=4
+#SBATCH --mail-type=end
+#SBATCH --mail-user=keropyan@in.tum.de
+#SBATCH --export=NONE
+#SBATCH --time=00:02:00
+
+
+module load slurm_setup
+
+
+module unload intel-mpi
+module unload intel
+module load gcc/11.2.0
+module load r/4.0.5-gcc11-mkl
+module load gsl/2.7-gcc11
+
+cd src/
+
+SAMPLE_SIZE=("10" "20")
+NUM_DATASETS=("20", "30")
+
+# run jobs
+# for n in "${SAMPLE_SIZE[@]}"; do
+#	for num_d in "${NUM_DATASETS[@]}"; do
+#		while true; do
+#			SUBJOBS= `jobs -r | wc -l` # detect how manv subiobs are alreadv runnina
+#			if test $SUBJOBS -It $SLURM_JOB_NUM_NODES; then
+#				sleep 1
+#				srun -N 1 -n $SLURM_NTASKS_PER_NODE -c $SLURM_CPUS_PER_TASK Rscript ../run_ltm.R n dum_d gaussian cube smoothed
+#			fi
+#		done
+#	done
+#done
+
+
+Rscript ../run_ltm.R 10 7 gaussian cube smoothed
+
+# query status for my job, approx. time for pending job, past jobs
+# squeue -M cm2 -u ge79mok2
+# squeue -M cm2 -u ge79mok2 --start
+# sacct -X -M cm2 -u ge79mok2
+
+# abort a job
+# scancel -M mpp2 <jobid>
+
+# see the busy cluster segments
+# sinfo
+
+# see available clusters
+# sacctmgr list clusters
+
+# slow cluster (serial, serial_std)
